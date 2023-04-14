344da141d8a901e61d084ff3d2756e9fe41d1a22
astanelis <113136627+astanelis@users.noreply.github.com>
2023-04-14 11:33:38 +0300

    Add files via upload

---
+++ app/book.js
@@ -0,0 +1,10 @@
+class Book {
+  constructor(title, author, category, year, price, image) {
+    this.title = title;
+    this.author = author;
+    this.category = category;
+    this.year = year;
+    this.price = price;
+    this.image = image;
+  }
+}
