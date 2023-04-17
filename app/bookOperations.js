f9bd74106b4e841030704064242d4e5e2f10d744
astanelis <stanelisa@gmail.com>
2023-04-17 21:39:22 +0300

    commit

--- app/bookOperations.js
+++ app/bookOperations.js
@@ -1,23 +1,59 @@
 function addBook(book) {
-    const books = getBooks();
-    books.push(book);
-    saveBooks(books);
-    displayBooks();
+  const books = getBooks()
+  books.push(book)
+  saveBooks(books)
+  displayBooks()
+}
+
+function deleteBook(index) {
+  const books = getBooks()
+  books.splice(index, 1)
+  saveBooks(books)
+  displayBooks()
+}
+
+function displayBooks(booksToDisplay = getBooks()) {
+  let html = ''
+
+  booksToDisplay.forEach((book, index) => {
+    html += `
+                <li class="swiper-slide booklist-item">
+                    <img src="${book.image}" alt="${book.title}" width="100" class="book-image">
+                    <h3 class="book-title">${book.title}</h3>
+                    <p class="book-author">Author: ${book.author}</p>
+                    <p class="book-category">Category: ${book.category}</p>
+                    <p class="book-year">Year: ${book.year}</p>
+                    <p class="book-price">Price: $${book.price}</p>
+                    <div class="button-container">
+                <button class="generated-button" onclick="deleteBook(${index})">Delete</button>
+                <button class="generated-button" onclick="editBook(${index})">Edit</button>
+            </div>
+                </li>
+            `
+  })
+
+  bookList.innerHTML = html
+
+  initSwiper()
+
+  const nextButton = document.querySelector('.swiper-button-next')
+  const prevButton = document.querySelector('.swiper-button-prev')
+
+  if (booksToDisplay.length > 0) {
+    nextButton.style.display = 'flex'
+    prevButton.style.display = 'flex'
+  } else {
+    nextButton.style.display = 'none'
+    prevButton.style.display = 'none'
   }
-  
-  function deleteBook(index) {
-    const books = getBooks();
-    books.splice(index, 1);
-    saveBooks(books);
-    displayBooks();
-  }
-  
-  function editBook(index) {
-    const books = getBooks();
-    const book = books[index];
-    const listItem = bookList.children[index];
-  
-    listItem.innerHTML = `
+}
+
+function editBook(index) {
+  const books = getBooks()
+  const book = books[index]
+  const listItem = bookList.children[index]
+
+  listItem.innerHTML = `
           <form class="edit-form">
               <img src="${book.image}" alt="${book.title}" width="100" class="book-image">
               <label>Title:</label>
@@ -35,57 +71,21 @@ function addBook(book) {
               <button type="submit" class="generated-button">Save</button>
               <button type="button" class="generated-button" onclick="displayBooks()">Cancel</button>
           </form>
-      `;
-  
-    const editForm = listItem.querySelector('.edit-form');
-    editForm.addEventListener('submit', function updateBook(event) {
-      event.preventDefault();
-  
-      book.title = listItem.querySelector('.edit-title').value;
-      book.author = listItem.querySelector('.edit-author').value;
-      book.category = listItem.querySelector('.edit-category').value;
-      book.year = listItem.querySelector('.edit-year').value;
-      book.price = listItem.querySelector('.edit-price').value;
-      book.image = listItem.querySelector('.edit-image').value;
-  
-      books[index] = book;
-      saveBooks(books);
-      displayBooks();
-    });
-  }
-  
-  function displayBooks(booksToDisplay = getBooks()) {
-    let html = '';
-  
-    booksToDisplay.forEach((book, index) => {
-      html += `
-                <li class="swiper-slide booklist-item">
-                    <img src="${book.image}" alt="${book.title}" width="100" class="book-image">
-                    <h3 class="book-title">${book.title}</h3>
-                    <p class="book-author">Author: ${book.author}</p>
-                    <p class="book-category">Category: ${book.category}</p>
-                    <p class="book-year">Year: ${book.year}</p>
-                    <p class="book-price">Price: $${book.price}</p>
-                    <div class="button-container">
-                <button class="generated-button" onclick="deleteBook(${index})">Delete</button>
-                <button class="generated-button" onclick="editBook(${index})">Edit</button>
-            </div>
-                </li>
-            `;
-    });
+      `
 
-    bookList.innerHTML = html;
+  const editForm = listItem.querySelector('.edit-form')
+  editForm.addEventListener('submit', function updateBook(event) {
+    event.preventDefault()
 
-    initSwiper();
-  
-    const nextButton = document.querySelector('.swiper-button-next');
-    const prevButton = document.querySelector('.swiper-button-prev');
-  
-    if (booksToDisplay.length > 0) {
-      nextButton.style.display = 'flex';
-      prevButton.style.display = 'flex';
-    } else {
-      nextButton.style.display = 'none';
-      prevButton.style.display = 'none';
-    }
-  }
+    book.title = listItem.querySelector('.edit-title').value
+    book.author = listItem.querySelector('.edit-author').value
+    book.category = listItem.querySelector('.edit-category').value
+    book.year = listItem.querySelector('.edit-year').value
+    book.price = listItem.querySelector('.edit-price').value
+    book.image = listItem.querySelector('.edit-image').value
+
+    books[index] = book
+    saveBooks(books)
+    displayBooks()
+  })
+}
